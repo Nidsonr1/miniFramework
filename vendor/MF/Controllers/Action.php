472a83b58073c91c5a467232b0d39f30e0cf36f8
Nidsonr1 <nidsoncampos@hotmail.com>
2021-01-11 23:02:25 -0300

    Definindo e reutilizando Layouts

--- vendor/MF/Controllers/Action.php
+++ vendor/MF/Controllers/Action.php
@@ -11,12 +11,20 @@ abstract class Action {
 		$this->view = new \stdClass();
   }
   
-  protected function render($view)
+  protected function render($view, $layout)
+  { 
+    $this->page = $view;
+    
+    file_exists("../App/Views/".$layout.".php") ? require_once "../App/Views/".$layout.".php" : $this->content();
+    
+  }
+
+  protected function content()
   {
     $currentClass = get_class($this);
     $currentClass = str_replace('App\\Controllers\\', '', $currentClass);
     $currentClass = strtolower(str_replace('Controller', '', $currentClass));
 
-    require_once "../App/Views/".$currentClass."/".$view.".php";
+    require_once "../App/Views/".$currentClass."/".$this->page.".php";
   }
 }
